3d38c846fedfe74cd8d1fe5252400e8dcd0f80bc
sdlima4025 <sergiolimafg3@gmail.com>
2023-06-06 10:39:21 -0300

    consultando documentação tailwind

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-black text-red-800 p-4 text-3xl font-bold">
-      Texto qualquer aqui
-      <button className=" bg-green-300 rounded-2xl text-2xl w-48">Save Changes</button>
+
+    <div className="bg-blue-300 p-5 ">
+      <input 
+      className=" outline-none border-transparent bg-white p-3 rounded-md focus:border-sky-500 focus:ring-2 focus:ring-sky-500"
+      type="text" placeholder="Digite alguma coisa"/>
+      <button className="ml-2 p-3 bg-sky-400 hover:bg-sky-600 rounded-md font-bold text-white">
+        clique aqui!
+      </button>
     </div>
   );
 }
